e716bf1c25f7120a0db5b80d86f24984205f72c8
Rychard.Famichou <rychfamich@gmail.com>
2023-08-04 09:45:45 +0200

    Add TestSwaggerPetCRUD.py

--- QA/Python/import requests.py
+++ QA/Python/import requests.py
@@ -1,6 +1,11 @@
 import requests
+import unittest
 import json
-body = '{"id": 1, "category": {"id": 1, "name": "good"}, "name": "good", "photoUrls": ["good"],"tags": [{"id": 1, "name": "good"}], "status": "good"}'
+body = '{"id": 4512, "category": {"id": 1, "name": "good"}, "name": "good", "photoUrls": ["good"],"tags": [{"id": 1, "name": "good"}], "status": "good"}'
 body_json = json.loads(body)
 r = requests.post('https://petstore.swagger.io/v2/pet', json=body_json)
+print(r.status_code)
+r = requests.get('https://petstore.swagger.io/v2/pet/4512')
+print(r.status_code)
+r = requests.delete('https://petstore.swagger.io/v2/pet/4512')
 print(r.status_code)
